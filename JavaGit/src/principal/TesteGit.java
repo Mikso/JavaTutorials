af1cc55e79827ad5e57a64b6838dcdb5b762d9d8
Mikso <bmarcos330@gmail.com>
2017-06-19 00:49:07 -0300

    First test

---
+++ JavaGit/src/principal/TesteGit.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package principal;
+
+/**
+ * @author Bruno
+ *
+ */
+public class TesteGit {
+
+	/**
+	 * 
+	 */
+	public TesteGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este é um teste para checkar se a equivalência entre o Eclipse e o GitHub");
+	}
+
+}
