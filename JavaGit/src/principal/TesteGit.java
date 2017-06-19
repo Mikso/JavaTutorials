ae2a0da4b0b1bcb4f8cc4fd6bd22a05eeb77e524
Mikso <bmarcos330@gmail.com>
2017-06-19 01:14:39 -0300

    First test - Alteration

--- JavaGit/src/principal/TesteGit.java
+++ JavaGit/src/principal/TesteGit.java
@@ -21,7 +21,8 @@ public class TesteGit {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Este é um teste para checkar se a equivalência entre o Eclipse e o GitHub");
+		System.out.println("Este é um teste para checkar se a equivalencia entre o Eclipse e o GitHub");
+		System.out.println("TUDO FUNCIONANDO");
 	}
 
 }
