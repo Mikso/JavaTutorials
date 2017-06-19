fbc369f66ceb48f6480b18b3df93e75cf8c23a02
Mikso <bmarcos330@gmail.com>
2017-06-19 01:34:39 -0300

    New program in JavaGit

---
+++ JavaGit/src/principal/PosTeste.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package principal;
+
+/**
+ * @author Bruno
+ * @see Git hub
+ * @version 1.0
+ * {@value Um teste simples}
+ */
+public class PosTeste {
+	String nome	=	null;
+	
+	/**
+	 * 
+	 */
+	public PosTeste() {
+		// TODO Auto-generated constructor stub
+		System.out.println(nome);
+	}
+	
+	void metodo(String nome){
+		this.nome="bruno";
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
